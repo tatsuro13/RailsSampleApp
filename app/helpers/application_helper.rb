7e1273bfb4a4183559438597f3d539628ab57506
ad_osuka_t <osuka_t@a-drop.com>
2018-09-28 17:37:34 +0900

    Deviseの動線追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
   def flash_message(message, klass)
     content_tag(:div, class: "alert alert-#{klass}") do
       concat content_tag(:button, 'x', class: 'close', data: { dismiss: 'alert' })
-      concat rae(message)
+      concat raw(message)
     end
   end
 end
